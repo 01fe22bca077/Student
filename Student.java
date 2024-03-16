bc2db948dac2e76cd8d2dba1994a8e14e709cdab
01fe22bca077 <01fe22bca077@kletech.ac.in>
2024-03-16 21:08:57 +0530

    Student

---
+++ Student.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Student {
+    private String name;
+    private String dob;
+
+    public Student(String name, String dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public void displayName() {
+        System.out.println("Student Name: " + name);
+    }
+
+    public void displayAge() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate dobDate = LocalDate.parse(dob, formatter);
+        LocalDate today = LocalDate.now();
+        Period age = Period.between(dobDate, today);
+        System.out.println("Age of " + name + ": " + age.getYears() + " years");
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("John Doe", "15-08-2000");
+        student1.displayName();
+        student1.displayAge();
+    }
+}
